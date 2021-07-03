9f4effc91d7083c9264ae0b038460f846974bda4
Jimmy <mohamed.sadek1011@gmail.com>
2021-07-03 10:23:29 +0200

    again again

--- website/work.py
+++ website/work.py
@@ -117,6 +117,7 @@ def move():
             sleep(0.01)
             ser.write(bytes(FanSpeed,'utf-8'))
             sleep(0.1)
+            return ('', 204)
         elif request.form['action'] == 'Water':
             WaterTime = str(request.form.get('Water_Time'))
             ser.write(b'pumb')
